f97488ecf43f403e3763e6b0c2a6c0ac8a94968b
mehas <ethan.mehas@gmail.com>
2016-09-15 15:14:21 -0400

    Begin homepage

--- src/components/common/widget/index.php
+++ src/components/common/widget/index.php
@@ -1,5 +1,5 @@
 <?php require( $_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); //begin base template ?>
-<h1>Test Component</h1>
+<h1>Example Component</h1>
 <div class="btn-group" role="group">
   <button type="button" class="btn btn-default">TV</button>
   <button type="button" class="btn btn-default">Internet</button>
