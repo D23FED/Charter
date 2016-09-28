cc75515233f1dd5685dcdd2fcbd67a8c9416a15e
mehas <ethan.mehas@gmail.com>
2016-09-28 10:25:37 -0400

    Example component

--- src/components/common/widget/index.php
+++
@@ -1,8 +0,0 @@
-<?php require( $_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); //begin base template ?>
-<h1>Example Component</h1>
-<div class="btn-group" role="group">
-  <button type="button" class="btn btn-default">TV</button>
-  <button type="button" class="btn btn-default">Internet</button>
-  <button type="button" class="btn btn-default">Phone</button>
-</div>
-<?php require($site['files']['site_footer']); //end base template ?>
