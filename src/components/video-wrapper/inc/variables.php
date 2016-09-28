9d787c26d57edeeb7074eb9e50e0afa78971253b
mehas <ethan.mehas@gmail.com>
2016-09-28 10:54:06 -0400

    Add video wrapper component

---
+++ src/components/video-wrapper/inc/variables.php
@@ -0,0 +1,17 @@
+<?php
+$document = array(
+	'title' => 'Video Wrapper',
+	'body'  => array(
+		'id'    => '',
+		'class' => ''
+	),
+	'component' => array(
+		'name' => 'Video Wrapper',
+		'lob'  => 'common' //common, residental, business, or enterprise
+	),
+	'site_header'    => true,
+	'site_footer'    => true,
+	'primary_nav'    => true,
+	'site_container' => 'fixed' // 'fixed' or 'full-width'
+);
+
