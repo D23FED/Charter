af5413c20d3811c37db09b59d0eb0e185fc252cb
mehas <ethan.mehas@gmail.com>
2016-10-18 15:38:26 -0400

    Add checkmark list style

---
+++ dist/components/list/index.php
@@ -0,0 +1,9 @@
+<?php require( $_SERVER[ 'DOCUMENT_ROOT']. '/inc/header.php'); //begin base template ?>
+<ul class="bullets-checkmark">
+	<li>125+ channels Including FREE HD</li>
+	<li>Access 10,000+ On Demand choices - including HD and movies in 3D</li>
+	<li>Stream live TV and On Demand content with the Spectrum TV App</li>
+	<li>Access your favorite network TV apps</li>
+	<li>Optional DVR service can be added on up to 4 TVs in your home – up to 780 hours of storage</li>
+</ul>
+<?php require($site[ 'files'][ 'site_footer']); //end base template ?>
