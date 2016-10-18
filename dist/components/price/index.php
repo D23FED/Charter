d1e3b41fcd87d9a247d73b3183ce1df54a8eaf48
mehas <ethan.mehas@gmail.com>
2016-10-18 18:16:12 -0400

    Updates for accordion table and pricing

--- dist/components/price/index.php
+++ dist/components/price/index.php
@@ -1,7 +1,7 @@
 <?php require( $_SERVER[ 'DOCUMENT_ROOT']. '/inc/header.php'); //begin base template ?>
 <?php
 $price_internet = array(
-	"before" => "from",
+	"before" => "From",
 	"after" => "",
 	"currency" => "$",
 	"whole" => "29",
@@ -22,7 +22,7 @@ function price_html($price, $class ='') { ?>
 
 		<?php // Before
 		if ( $price['before'] !== '') :?>
-			<div class="before p-row">
+			<div class="before row">
 				<div class="inner">
 					<?php echo $price['before']; ?>
 				</div>
@@ -30,28 +30,32 @@ function price_html($price, $class ='') { ?>
 		<?php endif;?>
 
 		<?php // Price ?>
-		<div class="price p-row">
-			<div class="currency p-col">
-				<?php echo $price['currency']; ?>
-			</div>
-			<div class="whole p-col">
-				<?php echo $price['whole']; ?>
-			</div>
-			<div class="details p-col">
-				<div class="inner">
-					<div class="super">
-						<?php echo $price['super']; ?>
-					</div>
-					<div class="term">
-						<?php echo $price['terms']; ?>
+		<div class="row-price row">
+			<div class="price">
+
+				<div class="currency col-price">
+					<?php echo $price['currency']; ?>
+				</div>
+				<div class="whole col-price">
+					<?php echo $price['whole']; ?>
+				</div>
+				<div class="details col-price">
+					<div class="inner">
+						<div class="super">
+							<?php echo $price['super']; ?>
+						</div>
+						<div class="term">
+							<?php echo $price['terms']; ?>
+						</div>
 					</div>
 				</div>
+
 			</div>
 		</div>
 
 		<?php // After
 		if ( $price['after'] !== '') :?>
-			<div class="after p-row">
+			<div class="after row">
 				<div class="inner">
 					<?php echo $price['after']?>
 				</div>
@@ -63,39 +67,34 @@ function price_html($price, $class ='') { ?>
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12">
-			<h1>Single Price</h1>
+			<h1>Price Component</h1>
+			<h2>Single Price</h2>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-6">
-			<h2>Basic</h2>
+			<h3>Basic</h3>
 			<?php price_html($price_internet); ?>
 		</div>
 		<div class="col-xs-6">
-			<h2>Style 01</h2>
+			<h3>Style 01</h3>
 			<?php price_html($price_internet, 'price-style-01'); ?>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12">
-			<h1>Double Price Block</h1>
+			<h2>Double Price Block</h2>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12">
 			<div class="price-block-double price-style-01">
 				<?php price_html($price_tv, 'price-style-01'); ?>
-				<span class="separator price-style-01">
-					&plus;
-				</span>
+				<span class="separator price-style-01">&plus;</span>
 				<?php price_html($price_internet, 'price-style-01'); ?>
 			</div>
 		</div>
 	</div>
 </div>
-
-
-
-
 
 <?php require($site[ 'files'][ 'site_footer']); //end base template ?>
