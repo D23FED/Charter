fe9ccc1066e719f72c53bbe62d5e0b6af575c26a
mehas <ethan.mehas@gmail.com>
2016-10-26 16:37:48 -0400

    Initial resi buttons styles

---
+++ src/sandbox/buttons/index.php
@@ -0,0 +1,27 @@
+<?php require( $_SERVER[ 'DOCUMENT_ROOT']. '/inc/header.php'); //begin base template ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<a class="btn" href="#">Button-style Link</a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<button>True &lt;button&gt;</button>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<form action="#">
+				<input type="button" value="Launch Nukes">
+				<input type="submit" value="Submit">
+			</form>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12"></div>
+	</div>
+</div>
+
+<?php require($site[ 'files'][ 'site_footer']); //end base template ?>
