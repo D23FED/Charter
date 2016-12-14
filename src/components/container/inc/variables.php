d00abe058d1cc1628058315eb3451c94c309d5c4
mehas <ethan.mehas@gmail.com>
2016-12-14 12:45:33 -0500

    Update boxed container styling

---
+++ src/components/container/inc/variables.php
@@ -0,0 +1,18 @@
+<?php
+$document = array(
+	'title' => 'Container and Boxes',
+	'body'  => array(
+		'id'    => '',
+		'class' => ''
+	),
+	'component' => array(
+		'name' => 'Container',
+		'lob'  => 'common', //common, residental, business, or enterprise
+		'lob_switch' => false //enable switching between several lines of business for a single component
+	),
+	'site_header'    => false,
+	'site_footer'    => false,
+	'primary_nav'    => false,
+	'site_container' => 'fixed' // 'fixed' or 'full-width'
+);
+
