3929628e70ee61e550cc6c6733bc07491a4639b5
mehas <ethan.mehas@gmail.com>
2017-02-20 13:29:56 -0500

    Build business typography demo markup

---
+++ src/bus/sandbox/typography/inc/content.php
@@ -0,0 +1,76 @@
+<h6>Headings</h6>
+<div class="example">
+	<h1>H1 Heading</h1>
+	<h2>H2 Heading</h2>
+	<h3>H3 Heading</h3>
+	<h4>H4 Heading</h4>
+	<h5>H5 Heading</h5>
+	<h6>H6 Heading</h6>
+</div>
+<hr>
+<h6>Primary Body Text</h6>
+<p class="example">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+<h6>Secondary Body Text</h6>
+<p class="small example">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+<h6>Subhead Paragraph Text</h6>
+<p class="lead example">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+<h6>Bullet Text</h6>
+<ul class="example">
+	<li>Bullet point</li>
+	<li>Bullet point</li>
+	<li>Bullet point</li>
+</ul>
+<ul class="small example">
+	<li>Bullet point</li>
+	<li>Bullet point</li>
+	<li>Bullet point</li>
+</ul>
+<h6>Image Caption</h6>
+<div class="caption example">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</div>
+<h6>Disclaimer</h6>
+<div class="disclaimer example">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</div>
+<h6>Disclaimer, small</h6>
+<div class="disclaimer small example">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</div>
+<hr>
+<h6>Semibold text</h6>
+<div class="example">
+	<h2>H2 Heading</h2>
+	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+<h2 class="font-semibold">H2 Heading</h2>
+<p class="font-semibold">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+</div>
+
+
+<!-- -->
+<h6>Bold text</h6>
+<div class="example">
+	<h2>H2 Heading</h2>
+	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+	<h2 class="font-bold">H2 Heading</h2>
+	<p class="font-bold">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+</div>
+<!-- -->
+<h6>Caps text</h6>
+<div class="example">
+	<h2>H2 Heading</h2>
+	<h2 class="text-uppercase">H2 Heading</h2>
+</div>
+<!-- -->
+<h6>White text</h6>
+<div class="example">
+	<h2>H2 Heading</h2>
+	<h2 class="text-white" style="background-color: black; display: inline-block;">H2 Heading</h2>
+</div>
+<!-- -->
+<h6>Teal text</h6>
+<div class="example">
+	<h2>H2 Heading</h2>
+	<h2 class="text-teal">H2 Heading</h2>
+</div>
+<!-- -->
+<h6>Blue text</h6>
+<div class="example">
+	<h4>h4 Heading</h4>
+	<h4 class="text-blue">h4 Heading</h4>
+</div>
+<!-- -->
