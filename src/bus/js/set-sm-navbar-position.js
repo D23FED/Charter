09b7c011f79f729b5d4edd7eb26118a6fc4c19e1
mehas <ethan.mehas@gmail.com>
2017-03-15 12:28:26 -0400

    Update GE #3

---
+++ src/bus/js/set-sm-navbar-position.js
@@ -0,0 +1,25 @@
+/* global $, _ */
+// Set top of hamburger menu to bottom of page header
+(function($) {
+	$(function() {
+		// Grab height of page header
+		$header = $('.menu-row-main');
+		$nav = $('.navbar-sm .navbar-collapse');
+		headerH = $header.outerHeight();
+
+		function setMenuTop() {
+			if ($header.length > 0 && $nav.length > 0) {
+				$nav.css('top', headerH);
+			} else {
+				console.error("Spectrum: Page header or hamburger menu not found");
+			}
+		}
+		// Call once at load
+		setMenuTop();
+		// Call on resize
+		$(window).on('resize', function() {
+			_.debounce(setMenuTop, 150);
+			console.log('blarbity');
+		});
+	});
+})(jQuery);
