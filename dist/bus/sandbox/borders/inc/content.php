4337f0c0b0207f522c98820a298e2f3fbd141fad
mehas <ethan.mehas@gmail.com>
2017-03-20 13:35:33 -0400

    Refine GE 2 and 3, add helpers

---
+++ dist/bus/sandbox/borders/inc/content.php
@@ -0,0 +1,14 @@
+<h6 class="example-header">Borders</h6>
+<div class="example">
+	<p><code>.bd-r5</code></p>
+	<div class="bd-r5 bd-1 bd-gray">
+		<p>Our fastest speed available – 50x faster than 6 Mbps DSL. Maximize productivity for all of your employees. Keep all of your employee and customer devices connected without any lag or slowdowns.</p>
+	</div>
+	<h1>H1 Heading</h1>
+	<h2>H2 Heading</h2>
+	<h3>H3 Heading</h3>
+	<h4>H4 Heading</h4>
+	<h5>H5 Heading</h5>
+	<h6>H6 Heading</h6>
+</div>
+<hr>
---
+++ src/bus/sandbox/borders/index.php
@@ -0,0 +1,13 @@
+<?php require($_SERVER['DOCUMENT_ROOT'].'/template/inc/header.php');
+//begin base template ?>
+<div class="container"></div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-10 col-sm-push-sm-1 col-md-8 col-md-push-2">
+			<h1>Typography: Spectrum Business</h1>
+			<hr>
+			<?php include('inc/content.php'); ?>
+		</div>
+	</div>
+</div>
+<?php require($site[ 'files'][ 'site_footer']);
+//end base template ?>
